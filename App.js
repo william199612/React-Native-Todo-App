bd2d6ac37948af5bfb04fd1cc3d838221fa538f4
William Hung <william851214@gmail.com>
2024-05-07 18:35:04 +1000

    (chore) Update default app and install dependencies

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import { AntDesign } from '@expo/vector-icons';
+
+// screen
+import Login from './screens/Login';
+import Signup from './screens/Signup';
+import Settings from './screens/Settings';
+
+// const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<Signup />
+		// <NavigationContainer>
+		// 	<Tab.Navigator
+		// 		screenOptions={({ route }) => {
+		// 			return {
+		// 				tabBarIcon: ({ color, size }) => {
+		// 					let iconName;
+		// 					if (route.name === 'Calendar') {
+		// 						iconName = 'calendar';
+		// 					} else if (route.name === 'Todo') {
+		// 						iconName = 'cog';
+		// 					} else if (route.name === 'Add') {
+		// 						iconName = 'plus';
+		// 					} else if (route.name === 'About') {
+		// 						iconName = 'about';
+		// 					} else if (route.name === 'Settings') {
+		// 						iconName = 'cog';
+		// 					}
+		// 					return <FontAwesome5 name={iconName} size={size} color={color} />;
+		// 				},
+		// 			};
+		// 		}}
+		// 	>
+		// 		{/* <Tab.Screen name="Calendar" component={} />
+		//     <Tab.Screen name="Todo" component={} />
+		//     <Tab.Screen name="Add" component={} />
+		//     <Tab.Screen name="About" component={} />
+		// 		<Tab.Screen name="Settings" component={SettingsScreen} /> */}
+		// 	</Tab.Navigator>
+		// 	<View style={styles.container}>
+		// 		<AntDesign name="checkcircle" size={24} color="black" />
+		// 		<Text>Just Do It!</Text>
+		// 	</View>
+		// </NavigationContainer>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
