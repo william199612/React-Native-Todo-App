4e2cf75965f92854e22a89bab9bd5b4c97da1e55
William Hung <william851214@gmail.com>
2024-05-15 23:10:49 +1000

    (feat) Redesign calendar screen

--- app/components/Task.jsx
+++ app/components/Task.jsx
@@ -1,52 +1,67 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { Card, Text } from 'react-native-paper';
 import { Colors } from './styles';
 
-const Task = ({ text }) => {
+const Task = ({ data }) => {
+	const handleComplete = () => {
+		data.isCompleted = !data.isCompleted;
+	};
+
 	return (
-		<View style={styles.item}>
-			<View style={styles.itemLeft}>
-				<TouchableOpacity style={styles.square}></TouchableOpacity>
-				<Text style={styles.itemText}>{text}</Text>
-			</View>
-			<View style={styles.circular}></View>
-		</View>
+		<Card style={styles.container}>
+			<Card.Content style={styles.item}>
+				<Text style={[styles.description, data.isCompleted && styles.completedText]}>{data.description}</Text>
+				<Text style={styles.time}>{data.dueTime}</Text>
+			</Card.Content>
+			<TouchableOpacity style={styles.taskContainer} onPress={handleComplete}>
+				<FontAwesome5
+					name={data.isCompleted ? 'check-circle' : 'circle'}
+					size={20}
+					color={data.isCompleted ? 'green' : 'grey'}
+				/>
+			</TouchableOpacity>
+		</Card>
 	);
 };
 
 const styles = StyleSheet.create({
-	item: {
-		backgroundColor: Colors.primary,
-		padding: 15,
-		borderRadius: 10,
+	container: {
+		position: 'relative',
+		marginVertical: 10,
 		flexDirection: 'row',
-		alignItems: 'center',
 		justifyContent: 'space-between',
-		marginBottom: 20,
-	},
-	itemLeft: {
-		flexDirection: 'row',
 		alignItems: 'center',
+		backgroundColor: 'white',
+		borderRadius: 10,
+	},
+	item: {
+		width: 400,
+		padding: 20,
+	},
+	description: {
 		flexWrap: 'wrap',
+		width: '80%',
+		fontSize: 18,
 	},
-	square: {
-		width: 24,
-		height: 24,
-		backgroundColor: Colors.brand,
-		opacity: 0.4,
-		borderRadius: 5,
-		marginRight: 15,
+	time: {
+		fontSize: 14,
+		color: Colors.tertiary,
+		marginTop: 10,
+		opacity: 0.5,
 	},
-	itemText: {
-		maxWidth: '80%',
+	taskContainer: {
+		position: 'absolute',
+		right: 10,
+		top: 0,
+		bottom: 0,
+		justifyContent: 'center',
+		width: 20,
+		opacity: 0.5,
 	},
-	circular: {
-		width: 12,
-		height: 12,
-		borderColor: Colors.brand,
-		borderRadius: 5,
-		borderWidth: 2,
-		borderRadius: 5,
+	completedText: {
+		textDecorationLine: 'line-through',
 	},
 });
 
--- app/screens/Calendar.jsx
+++ app/screens/Calendar.jsx
@@ -1,12 +1,68 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import { Calendar } from 'react-native-calendars';
 
-const Calendar = () => {
+import Task from '../components/Task';
+import { Colors } from '../components/styles';
+
+const todos = {
+	'2024-05-15': [
+		{ description: 'Eat', dueTime: '10:00', isCompleted: false },
+		{
+			description: 'Event Event Event Event Event Event Event Event Event Event Event',
+			dueTime: '10:01',
+			isCompleted: true,
+		},
+		{ description: 'Eat sleep and netflix and chill', dueTime: '10:02', isCompleted: true },
+		{ description: 'Event 2', dueTime: '10:03', isCompleted: true },
+		{ description: 'Event 2', dueTime: '10:04', isCompleted: true },
+		{ description: 'Event 2', dueTime: '10:05', isCompleted: true },
+		{ description: 'Event 2', dueTime: '10:06', isCompleted: true },
+		{ description: 'Event 2', dueTime: '10:07', isCompleted: true },
+		{ description: 'Event 2', dueTime: '10:08', isCompleted: true },
+	],
+	'2024-05-16': [{ title: 'Event 3' }, { title: 'Event 4' }],
+};
+
+const CalendarScreen = () => {
+	const [items, setItems] = useState(todos);
+	const [selectedDate, setSelectedDate] = useState(new Date().toISOString().split('T')[0]);
+
+	useEffect(() => {
+		setItems(todos);
+	}, [todos]);
+
+	const handleDayPress = (date) => {
+		setSelectedDate(date.dateString);
+	};
+
 	return (
-		<View>
-			<Text>Calendar</Text>
+		<View style={styles.container}>
+			<Calendar onDayPress={handleDayPress} markedDates={{ [selectedDate]: { selected: true } }} />
+			<Text>Selected Date: {selectedDate}</Text>
+			<ScrollView style={styles.scrollContainer}>
+				{items[selectedDate] && items[selectedDate].map((data, index) => <Task index={index} data={data} />)}
+			</ScrollView>
 		</View>
 	);
 };
 
-export default Calendar;
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+	scrollContainer: {
+		paddingHorizontal: 30,
+	},
+	item: {
+		backgroundColor: 'white',
+		flex: 1,
+		borderRadius: 5,
+		padding: 10,
+		marginRight: 10,
+		marginTop: 30,
+	},
+	theme: {},
+});
+
+export default CalendarScreen;
