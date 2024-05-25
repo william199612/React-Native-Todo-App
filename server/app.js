c5352f0f38f194ce100cdbef55e432a228060fce
William Hung <william851214@gmail.com>
2024-05-25 17:01:39 +1000

    (server) modified server app.js

--- server/app.js
+++ server/app.js
@@ -1,41 +1,67 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const knex = require("knex")(db);
+const fs = require("fs");
+const swaggerUI = require("swagger-ui-express");
+const swaggerDocument = require("./docs/openapi.json");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRoute = require("./routes/index");
 
-var app = express();
+const app = express();
+const logStream = fs.createWriteStream(
+	path.join(__dirname, "access.txt"),
+	{ flags: "a" }
+);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+// Middleware setup
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use((req, res, next) => {
+	req.db = knex;
+	next();
+});
+app.use(logger("common", { stream: logStream }));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Routes
+app.use("/", indexRoute);
+app.use("/todos", todoRoute);
+app.use("/users", userRoute);
+app.use("/version", (req, res) => {
+	req.db
+		.raw("SELECT VERSION()")
+		.then((version) => res.send(version[0][0]));
+});
+app.use(
+	"/docs",
+	swaggerUI.serve,
+	swaggerUI.setup(swaggerDocument)
+);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error =
+		req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render("error");
 });
 
 module.exports = app;
