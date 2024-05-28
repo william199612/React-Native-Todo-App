46e3904f541a7a926db4559cd9e87561f2320e92
William Hung <william851214@gmail.com>
2024-05-28 15:43:53 +1000

    (app) add darkmode style for settings screen

--- app/screens/Settings.jsx
+++ app/screens/Settings.jsx
@@ -13,16 +13,16 @@ const Settings = ({ navigation }) => {
 	};
 
 	return (
-		<View style={styles.container}>
+		<View style={theme === 'dark' ? styles.darkContainer : styles.lightContainer}>
 			<StatusBar style="dark" />
 			<View style={styles.innerContainer}>
 				<View style={styles.modeWrapper}>
-					<Text style={styles.modeText}>Dark Mode</Text>
+					<Text style={theme === 'dark' ? styles.darkText : styles.lightText}>Dark Mode</Text>
 					<Switch style={styles.switch} value={theme === 'dark'} onValueChange={toggleTheme} />
 				</View>
 
-				<Pressable style={styles.button} onPress={handleLogout}>
-					<Text style={styles.buttonText}>Logout</Text>
+				<Pressable style={theme === 'dark' ? styles.darkButton : styles.lightButton} onPress={handleLogout}>
+					<Text style={theme === 'dark' ? styles.darkButtonText : styles.lightButtonText}>Logout</Text>
 				</Pressable>
 			</View>
 		</View>
@@ -30,11 +30,17 @@ const Settings = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
-	container: {
+	lightContainer: {
 		flex: 1,
 		padding: 25,
 		paddingTop: 30,
 		backgroundColor: Colors.primary,
+	},
+	darkContainer: {
+		flex: 1,
+		padding: 25,
+		paddingTop: 30,
+		backgroundColor: Colors.tertiary,
 	},
 	innerContainer: {
 		flex: 1,
@@ -43,17 +49,24 @@ const styles = StyleSheet.create({
 	modeWrapper: {
 		flexDirection: 'row',
 	},
-	modeText: {
+	lightText: {
 		fontSize: 20,
 		fontWeight: 'bold',
 		textAlign: 'left',
 		width: '50%',
 	},
+	darkText: {
+		fontSize: 20,
+		fontWeight: 'bold',
+		textAlign: 'left',
+		width: '50%',
+		color: Colors.primary,
+	},
 	switch: {
 		width: '50%',
 		height: '50%',
 	},
-	button: {
+	lightButton: {
 		position: 'absolute',
 		bottom: 80,
 		width: '100%',
@@ -65,8 +78,24 @@ const styles = StyleSheet.create({
 		marginBottom: 10,
 		height: 50,
 	},
-	buttonText: {
+	darkButton: {
+		position: 'absolute',
+		bottom: 80,
+		width: '100%',
+		padding: 10,
+		backgroundColor: Colors.secondary,
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderRadius: 5,
+		marginBottom: 10,
+		height: 50,
+	},
+	lightButtonText: {
 		color: Colors.primary,
+		fontSize: 18,
+	},
+	darkButtonText: {
+		color: Colors.black,
 		fontSize: 18,
 	},
 });
